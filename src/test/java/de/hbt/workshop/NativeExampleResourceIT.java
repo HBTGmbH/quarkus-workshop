980a4ad94d3396bf57591bef2731d1d1b1fa7c1a
Niko Köbler <niko@n-k.de>
2020-09-21 10:32:18 +0200

    initial commit

---
+++ src/test/java/de/hbt/workshop/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package de.hbt.workshop;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
